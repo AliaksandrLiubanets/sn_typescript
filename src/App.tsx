ffb4f8160bf78e175da13e86504e7663be29aada
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-15 15:22:39 +0300

    Create Navbar component with Navbar.module.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,16 @@
 import React from 'react';
-import empty_avatar from './assets/empty_avatar.jpg'
 import './App.css';
 import Header from "./components/Header/Header";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
-  return (
-    <div className="App">
-        <Header />
-
-
-      <div className={'navbar'}>
-          <div>Profile
-          </div>
-          <div>Message</div>
-          <div>News</div>
-          <div>Music</div>
-          <div>Settings</div>
-      </div>
-      <div className={'content'}>Content</div>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header />
+            <Navbar />
+            <div className={'content'}>Content</div>
+        </div>
+    );
 }
 
 export default App;
---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import s from './Navbar.module.css'
+
+function Navbar() {
+    return <div className={s.navbar}>
+        <div>Profile
+        </div>
+        <div>Message</div>
+        <div>News</div>
+        <div>Music</div>
+        <div>Settings</div>
+    </div>
+}
+
+export default Navbar
