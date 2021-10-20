ca0339bb39fc4c4b3328046811f71993db9f9863
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-20 13:45:53 +0300

    Delete .item from every NavLink in Navbar.tsx

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,13 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 import s from './Navbar.module.css'
 
 function Navbar() {
     return <div className={s.navbar}>
-        <div><a href='/profile'>Profile</a></div>
-        <div><a href='/dialogs'>Dialogs</a></div>
-        <div><a href='/news'>News</a></div>
-        <div><a href='/music'>Music</a></div>
-        <div><a href='/settings'>Settings</a></div>
+        <div><NavLink to='/profile' activeClassName={s.active}>Profile</NavLink></div>
+        <div><NavLink to='/dialogs' activeClassName={s.active}>Dialogs</NavLink></div>
+        <div><NavLink to='/news' activeClassName={s.active}>News</NavLink></div>
+        <div><NavLink to='/music' activeClassName={s.active}>Music</NavLink></div>
+        <div><NavLink to='/settings' activeClassName={s.active}>Settings</NavLink></div>
     </div>
 }
 
