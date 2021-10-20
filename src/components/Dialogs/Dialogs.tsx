77e7c98160f1c35ea7780c0537388e79e7b323ff
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-20 11:29:42 +0300

    Create Dialogs component

---
+++ src/components/Dialogs/Dialogs.tsx
@@ -0,0 +1,7 @@
+function Dialogs() {
+    return <div>
+        Dialogs
+    </div>
+}
+
+export default Dialogs
--- src/components/Profile/AddPost/AddPost.tsx
+++ src/components/Profile/AddPost/AddPost.tsx
@@ -4,8 +4,9 @@ import s from './AddPost.module.css'
 
 function AddPost() {
     return <div className={s.addPost}>
-        <div>Add post:</div>
+        <div>My posts:</div>
         <textarea></textarea>
+        <button>Add post</button>
         <Posts post={'hello!'} likes={3}/>
         <Posts post={'Hi!'} likes={5}/>
         <Posts post={'How is it going?'} likes={10}/>
