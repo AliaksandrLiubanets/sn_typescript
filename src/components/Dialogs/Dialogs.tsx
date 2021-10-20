837a7bf2c98ed959a894f15264490336e259569e
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-20 14:06:52 +0300

    Fill Dialogs by dialog's names and messages

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,6 +1,20 @@
+import s from './Dialogs.module.css'
+
 function Dialogs() {
-    return <div>
-        Dialogs
+    return <div className={s.dialogs}>
+        <div className={s.dialogsItem}>
+            <div>Dimych</div>
+            <div>Andrew</div>
+            <div>Platon</div>
+            <div>Aleksandr</div>
+            <div>Maks</div>
+            <div>Sveta</div>
+        </div>
+        <div className={s.messages}>
+            <div>Hello!</div>
+            <div>How is it going?</div>
+            <div>Yo!</div>
+        </div>
     </div>
 }
 
