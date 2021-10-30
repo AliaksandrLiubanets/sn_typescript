6f9857260f33f1dabc4b56e407769b0b92c659fd
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-30 21:20:22 +0300

    Add grids for message_block in Message.module.css

--- src/components/Dialogs/Messages/Message.tsx
+++ src/components/Dialogs/Messages/Message.tsx
@@ -5,9 +5,11 @@ type MessagePropsType = {
     ava: string
 }
 function Message(props: MessagePropsType) {
-    return <div>
-        <div className={s.message_ava}><img src={props.ava} alt={'ava'}/></div>
-        <div className={s.message_name}>{props.name}</div>
+    return <div className={s.message_block}>
+        <div className={s.ava_name_message}>
+            <div className={s.message_ava}><img src={props.ava} alt={'ava'}/></div>
+            <div className={s.message_name}>{props.name}</div>
+        </div>
         <div className={s.message_massage}>{props.message}</div>
     </div>
 }
