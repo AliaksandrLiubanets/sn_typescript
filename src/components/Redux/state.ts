3598491524aa97152adb9131bd5016c41d3ba5fe
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-30 20:07:31 +0300

    Create avatars link for filling of dialogsData in state.ts

--- src/components/Redux/state.ts
+++ src/components/Redux/state.ts
@@ -1,8 +1,16 @@
 import {v1} from 'uuid'
 
+const ava_dimych = 'https://st.depositphotos.com/3335611/4577/i/950/depositphotos_45773129-stock-photo-samurai.jpg'
+const ava_andrew = 'https://disgustingmen.com/wp-content/uploads/2017/06/yasuke-vladimir-samoilov-art.jpg'
+const ava_lenin = 'https://media.istockphoto.com/vectors/samurai-emblem-vector-id531859980'
+const ava_pushkin = 'https://chto-eto-takoe.ru/uryaimg/samurai.JPG'
+const ava_dragunsky = 'https://e7.pngegg.com/pngimages/941/299/png-clipart-samurai-anime-drawing-samurai-black-hair-chibi.png'
+const ava_ostrovsky = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXFDqtEj-fqlEk4YI8fWneqfa2nPyvgl15Yg&usqp=CAU'
+
 export type dialogType = {
     id: string
     name: string
+    ava: string
 }
 
 export type messageType = {
@@ -31,12 +39,12 @@ const state = {
             {id: v1(), message: 'One more request!'}
         ] as Array<messageType>,
         dialogs: [
-            {id: v1(), name: 'Dimych'},
-            {id: v1(), name: 'Andrew'},
-            {id: v1(), name: 'Lenin'},
-            {id: v1(), name: 'Pushkin'},
-            {id: v1(), name: 'Dragunsky'},
-            {id: v1(), name: 'Ostrovskiy'}
+            {id: v1(), name: 'Dimych', ava: ava_dimych},
+            {id: v1(), name: 'Andrew', ava: ava_andrew},
+            {id: v1(), name: 'Lenin', ava: ava_lenin},
+            {id: v1(), name: 'Pushkin', ava: ava_pushkin},
+            {id: v1(), name: 'Dragunsky', ava: ava_dragunsky},
+            {id: v1(), name: 'Ostrovskiy', ava: ava_ostrovsky}
         ] as Array<dialogType>
     },
     sidebar: {}
