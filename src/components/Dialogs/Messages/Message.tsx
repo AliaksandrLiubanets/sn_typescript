fcf9f5f4b1e731a333169ac21a9cafa2448b21eb
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-10-31 13:06:50 +0300

    Add angle to message block

--- src/components/Dialogs/Messages/Message.tsx
+++ src/components/Dialogs/Messages/Message.tsx
@@ -10,6 +10,7 @@ function Message(props: MessagePropsType) {
             <div className={s.message_ava}><img src={props.ava} alt={'ava'}/></div>
             <div className={s.message_name}>{props.name}</div>
         </div>
+        <div className={s.angle}></div>
         <div className={s.message_massage}>{props.message}</div>
     </div>
 }
