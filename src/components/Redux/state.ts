87867f3fb632bc254c8e65a6a8c72c44602eb359
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-11-02 19:31:41 +0300

    Complete to fill state.dialogsPage.messages

--- src/components/Redux/state.ts
+++ src/components/Redux/state.ts
@@ -3,15 +3,22 @@ import {v1} from 'uuid'
 import ava_artem from '../../assets/ava_lenin.png'
 import ava_olga from '../../assets/ava_olga.jpg'
 import ava_karina from '../../assets/ava_karina.jpg'
+import ava_andrew from '../../assets/ava_andrew.jpg'
+import ava_lenin from '../../assets/ava_lenin.png'
+import ava_pushkin from '../../assets/ava_pushkin.jpg'
+import ava_dragunsky from '../../assets/ava_dragunsky.jpg'
+import ava_ostrovsky from '../../assets/ava_ostrovskiy.jpg'
+import ava_me from '../../assets/ava_me.jpg'
+import ava_dimych from '../../assets/ava_dimych.jpg'
 
 
-const ava_dimych = 'https://st.depositphotos.com/3335611/4577/i/950/depositphotos_45773129-stock-photo-samurai.jpg'
-const ava_andrew = 'https://disgustingmen.com/wp-content/uploads/2017/06/yasuke-vladimir-samoilov-art.jpg'
-const ava_lenin = 'https://media.istockphoto.com/vectors/samurai-emblem-vector-id531859980'
-const ava_pushkin = 'https://chto-eto-takoe.ru/uryaimg/samurai.JPG'
-const ava_dragunsky = 'https://e7.pngegg.com/pngimages/941/299/png-clipart-samurai-anime-drawing-samurai-black-hair-chibi.png'
-const ava_ostrovsky = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXFDqtEj-fqlEk4YI8fWneqfa2nPyvgl15Yg&usqp=CAU'
-const ava_me = 'https://avatars.githubusercontent.com/u/69769817?v=4'
+// const ava_dimych = 'https://st.depositphotos.com/3335611/4577/i/950/depositphotos_45773129-stock-photo-samurai.jpg'
+// const ava_andrew = 'https://disgustingmen.com/wp-content/uploads/2017/06/yasuke-vladimir-samoilov-art.jpg'
+// const ava_lenin = 'https://media.istockphoto.com/vectors/samurai-emblem-vector-id531859980'
+// const ava_pushkin = 'https://chto-eto-takoe.ru/uryaimg/samurai.JPG'
+// const ava_dragunsky = 'https://e7.pngegg.com/pngimages/941/299/png-clipart-samurai-anime-drawing-samurai-black-hair-chibi.png'
+// const ava_ostrovsky = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSXFDqtEj-fqlEk4YI8fWneqfa2nPyvgl15Yg&usqp=CAU'
+// const ava_me = 'https://avatars.githubusercontent.com/u/69769817?v=4'
 
 export type DialogType = {
     id: string
@@ -32,6 +39,15 @@ export type MessageType = {
     ava: string
 }
 
+type DialogsPageMessagesType = {
+    dimych: Array<MessageType>
+    andrew: Array<MessageType>
+    lenin: Array<MessageType>
+    pushkin: Array<MessageType>
+    dragunsky: Array<MessageType>
+    ostrovskiy: Array<MessageType>
+}
+
 export type PostType = {
     id: string
     message: string
@@ -47,11 +63,38 @@ const state = {
         ] as Array<PostType>
     },
     dialogsPage: {
-        messages: [
-            {id: v1(), message: 'Hello!', name: 'Dimych', ava: ava_dimych},
-            {id: v1(), message: 'How is your profile on LinkedIn?', name: 'Me', ava: ava_me },
-            {id: v1(), message: 'One more request!', name: 'Dimych', ava: ava_dimych}
-        ] as Array<MessageType>,
+        messages: {
+            dimych: [
+                {id: v1(), message: 'Hello!', name: 'Dimych', ava: ava_dimych},
+                {id: v1(), message: 'How is your profile on LinkedIn?', name: 'Me', ava: ava_me},
+                {id: v1(), message: 'One more request!', name: 'Dimych', ava: ava_dimych}
+            ] as Array<MessageType>,
+            andrew: [
+                {id: v1(), message: 'Hi, Andrew!', name: 'Me', ava: ava_me},
+                {id: v1(), message: 'Hi! Do you have a wife?', name: 'Andrew', ava: ava_andrew},
+                {id: v1(), message: 'Yes!', name: 'Me', ava: ava_me}
+            ] as Array<MessageType>,
+            lenin: [
+                {id: v1(), message: 'Good morning, mr. Lenin!', name: 'Me', ava: ava_me},
+                {id: v1(), message: 'Hi proletariy?', name: 'Lenin', ava: ava_lenin},
+                {id: v1(), message: 'Do you glad that made october revolution?', name: 'Me', ava: ava_me}
+            ] as Array<MessageType>,
+            pushkin: [
+                {id: v1(), message: 'Hello, my dear friend', name: 'Pushkin', ava: ava_pushkin},
+                {id: v1(), message: 'Good evening, Aleksandr Sergeevich!', name: 'Me', ava: ava_me},
+                {id: v1(), message: 'Old man and old woman lived for a long time on the shore of the blue sea... Do you remember this fairy tale? ', name: 'Pushkin', ava: ava_pushkin}
+            ] as Array<MessageType>,
+            dragunsky: [
+                {id: v1(), message: 'Hi, Sasha!', name: 'Dragunsky', ava: ava_dragunsky},
+                {id: v1(), message: 'How is your profile on LinkedIn?', name: 'Me', ava: ava_me},
+                {id: v1(), message: 'What is the most funny story of "Deniskiny rasskazy"?', name: 'Dragunsky', ava: ava_dragunsky}
+            ] as Array<MessageType>,
+            ostrovskiy: [
+                {id: v1(), message: 'What\'s your name!', name: 'Ostrovskiy', ava: ava_ostrovsky},
+                {id: v1(), message: 'Aleksander', name: 'Me', ava: ava_me},
+                {id: v1(), message: 'So, how do you think? How can steel be hardened?', name: 'Ostrovskiy', ava: ava_ostrovsky}
+            ] as Array<MessageType>
+        } as DialogsPageMessagesType,
         dialogs: [
             {id: v1(), name: 'Dimych', ava: ava_dimych},
             {id: v1(), name: 'Andrew', ava: ava_andrew},
@@ -65,7 +108,7 @@ const state = {
         friends: [
             {id: v1(), name: 'Artem', ava: ava_artem},
             {id: v1(), name: 'Olga', ava: ava_olga},
-            {id: v1(), name: 'Karina', ava: ava_karina},
+            {id: v1(), name: 'Karina', ava: ava_karina}
         ] as Array<FriendsType>
     }
 }
