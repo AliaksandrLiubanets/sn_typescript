dee0ee6f77a933e83b52b56aae4be9110f631640
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-11-30 13:59:54 +0300

    Wrap App with StoreContext.Provider

--- src/index.tsx
+++ src/index.tsx
@@ -4,12 +4,15 @@ import './index.css'
 import App from './App'
 import {BrowserRouter} from 'react-router-dom'
 import store, {rootStateType} from './components/Redux/redux-store'
+import {StoreContext} from './StoreContext/StoreContext'
 
 let rerenderEntireTree = (state: rootStateType) => {
     ReactDOM.render(
         <React.StrictMode>
             <BrowserRouter>
-                <App store={store} />
+                <StoreContext.Provider value={store}>
+                    <App />
+                </StoreContext.Provider>
             </BrowserRouter>
         </React.StrictMode>,
         document.getElementById('root')
