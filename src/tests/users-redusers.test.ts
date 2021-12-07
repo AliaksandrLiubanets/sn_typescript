e9861089405dc1f7e158e02307feb7c1b9117419
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-07 14:55:37 +0300

    Complete test: should unsubscribe from user (test usersReducer)

--- src/tests/users-redusers.test.ts
+++ src/tests/users-redusers.test.ts
@@ -1,4 +1,4 @@
-import usersReducer, {followAC, UsersStateType} from '../components/Redux/users-reducer'
+import usersReducer, {followAC, unfollowAC, UsersStateType} from '../components/Redux/users-reducer'
 import ava_dimych from '../assets/ava_dimych.jpg'
 import ava_olga from '../assets/ava_olga.jpg'
 import ava_karina from '../assets/ava_karina.jpg'
@@ -42,5 +42,18 @@ test('should subscribe to user', () => {
 
     expect(changedState.users[1].followed).toBe(true)
     expect(changedState.users[0].followed).toBe(true)
+    expect(Object.keys(changedState.users[1]).length).toBe(6)
+
+})
+
+test('should unsubscribe from user', () => {
+
+    const action = unfollowAC('3')
+    const changedState = usersReducer(state, action)
+
+    expect(changedState.users[0].followed).toBe(true)
+    expect(changedState.users[1].followed).toBe(false)
+    expect(changedState.users[2].followed).toBe(false)
+    expect(Object.keys(changedState.users[1]).length).toBe(6)
 
 })
