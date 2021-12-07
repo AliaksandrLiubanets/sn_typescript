0aa1fb220a8d87584ba24ebef3a73d8522456617
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-07 16:36:15 +0300

    Complete test: should add post (test profileReducer)

---
+++ src/tests/profile-redusers.test.ts
@@ -0,0 +1,40 @@
+import profileReducer, {
+    addCurrentValueAC,
+    addPostAC,
+    PostType,
+    ProfilePageType
+} from '../components/Redux/profile-reducer'
+
+let state: ProfilePageType
+
+beforeEach(() => {
+    state = {
+        textareaCurrentValue: '',
+        messagesData: [
+            {id: '1', message: 'hello!', likes: 3},
+            {id: '2', message: 'Hi!', likes: 5},
+            {id: '3', message: 'How is it going?!', likes: 8}
+        ]
+    }
+})
+
+test('should add post', () => {
+
+    state = {
+        textareaCurrentValue: 'Hello friend!',
+        messagesData: [
+            {id: '1', message: 'hello!', likes: 3},
+            {id: '2', message: 'Hi!', likes: 5},
+            {id: '3', message: 'How is it going?!', likes: 8}
+        ]
+    }
+
+    const action = addPostAC()
+    const changedState = profileReducer(state, action)
+
+    expect(changedState.messagesData[3]).toBeDefined()
+    expect(changedState.textareaCurrentValue).toBe('')
+    expect(changedState.messagesData[3].message).toBe('Hello friend!')
+})
+
+
