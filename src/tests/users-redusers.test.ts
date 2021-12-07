ffb920cc066cefa7d38f1bb650b70398e6f4e93e
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2021-12-07 14:41:35 +0300

    Complete test: should subscribe to user (test usersReducer)

--- src/tests/users-redusers.test.ts
+++ src/tests/users-redusers.test.ts
@@ -1,4 +1,7 @@
 import usersReducer, {followAC, UsersStateType} from '../components/Redux/users-reducer'
+import ava_dimych from '../assets/ava_dimych.jpg'
+import ava_olga from '../assets/ava_olga.jpg'
+import ava_karina from '../assets/ava_karina.jpg'
 
 let state: UsersStateType
 type UserType = {
@@ -18,7 +21,14 @@ type UserType = {
 
 beforeEach(() => {
     state = {
-        users: [],
+        users: [
+            {id: '1', followed: true, status: 'Life is good!', name: 'Dimych',
+                photos: {small: ava_dimych, large: ''}, location: {city: 'Minsk', country: 'Belarus'}},
+            {id: '2', followed: false, status: 'Life is good!', name: 'Olga',
+                photos: {small: ava_olga, large: ''}, location: {city: 'Minsk', country: 'Belarus'}},
+            {id: '3', followed: true, status: 'Life is good!', name: 'Karina',
+                photos: {small: ava_karina, large: ''}, location: {city: 'Minsk', country: 'Belarus'}}
+        ],
         totalCount: 0,
         pageSize: 5,
         currentPage: 2
@@ -27,9 +37,10 @@ beforeEach(() => {
 
 test('should subscribe to user', () => {
 
-    const action = followAC('-3-')
+    const action = followAC('2')
     const changedState = usersReducer(state, action)
 
-    expect(changedState)
+    expect(changedState.users[1].followed).toBe(true)
+    expect(changedState.users[0].followed).toBe(true)
 
 })
