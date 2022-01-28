afa50713a4ef255b4fbc579331223e1fe8cb87e0
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-01-28 20:36:14 +0300

    Add Preloader in ProfileInfo.tsx

--- src/components/Profile/ProfileInfo/ProfileInfo.tsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.tsx
@@ -1,16 +1,22 @@
 import samurai_background from '../../../assets/samurai_background.jpg'
 import s from './ProfileInfo.module.css'
 import {ProfileType} from '../../Redux/profile-reducer'
+import {Preloader} from '../../common/Preloader/Preloader'
 
 type ProfileInfoProps = {
     profile: ProfileType | null
 }
 
 export function ProfileInfo ({profile}: ProfileInfoProps) {
+
+    if (!profile) {
+        return <Preloader />
+    }
+
     return <div className={s.profileInfo}>
         <img src={samurai_background} alt="background"/>
         <div className={s.profileInfo_ava}>
-            <img src={!profile ? '' : profile.photos.small } alt="ava"/>
+            <img src={ profile.photos.small } alt="ava"/>
         </div>
         <div className={s.profileInfo_name}>
             <span>{profile && profile.fullName}</span>
