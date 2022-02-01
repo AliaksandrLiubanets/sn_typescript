9d1615b885db8381a4634f1c0171ccb8964314ad
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-01 18:17:48 +0300

    Add avatar for user in  Header.tsx

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,6 +1,7 @@
 import empty_avatar from "../../assets/empty_avatar.jpg";
 import s from './Header.module.css'
 import React from "react";
+import sancho_ava from '../../assets/ECGP3801_1.jpg'
 
 type HeaderProps = {
     login: string | null
@@ -15,7 +16,7 @@ export function Header({login, avatar, isAuth}: HeaderProps) {
             ?
             <div className={s.header_block}>
                 <div>
-                    <img src={''} alt={'ava'}/>
+                    <img src={!avatar ? sancho_ava : avatar} alt={'ava'}/>
                 </div>
                 <span>{login}</span>
             </div>
