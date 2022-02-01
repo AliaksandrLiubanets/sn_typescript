38e1459b3260b16453a4f5c188af41deb019cf1e
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-01 11:17:39 +0300

    Type AuthType,  create checkIsAuthorized in auth-reducer.ts

--- src/components/Redux/auth-reducer.ts
+++ src/components/Redux/auth-reducer.ts
@@ -1,54 +1,40 @@
 
-export const ADD_POST = 'sn-typescript/ProfilePage/ADD-POST'
+export const IS_AUTH = 'sn-typescript/Authorize/IS-AUTH'
 
-type AuthStateType = {
+type AuthDataType = {
     id: string | null
     email: string | null
     login: string | null
 }
 
-const initialState: AuthStateType = {
-    id: null,
-    email: null,
-    login: null,
+type AuthStateType = {
+    data: AuthDataType
+    isAuth: boolean
 }
 
-const profileReducer = (state = initialState, action: ProfilePageActionsType): ProfilePageType => {
+const initialState: AuthStateType = {
+    data: {
+        id: null,
+        email: null,
+        login: null,
+    },
+    isAuth: false,
+}
+
+const authReducer = (state: AuthStateType = initialState, action: AuthActionsType): AuthStateType => {
     switch (action.type) {
-        case ADD_POST:
-            let newPost: PostType
-            newPost = {
-                id: v1(),
-                message: state.textareaCurrentValue.trim(),
-                likes: 7
-            }
+        case IS_AUTH:
+            return {...state, data: action.data}
 
-            if (newPost.message) {
-                return {
-                    ...state,
-                    textareaCurrentValue: '',
-                    messagesData: [...state.messagesData, newPost]
-                }
-            }
-
-            return state
-        case ADD_CURRENT_VALUE:
-            return {...state, textareaCurrentValue: action.newText}
-        case SET_USER_PROFILE:
-            return {...state, profile: action.profile}
         default:
             return state
     }
 }
 
-export type AddPostActionType = ReturnType<typeof addPost>
-export type AddCurrentValueActionType = ReturnType<typeof addCurrentValue>
-export type SetUserProfileActionType = ReturnType<typeof setUserProfile>
+export type AuthType = ReturnType<typeof checkIsAuthorized>
 
-export type ProfilePageActionsType = AddPostActionType | AddCurrentValueActionType | SetUserProfileActionType
+export type AuthActionsType = AuthType
 
-export const addPost = () => ({type: ADD_POST} as const)
-export const addCurrentValue = (text: string) => ({type: ADD_CURRENT_VALUE, newText: text} as const)
-export const setUserProfile = (profile: ProfileType) => ({type: SET_USER_PROFILE, profile} as const)
+export const checkIsAuthorized = (data: AuthDataType) => ({type: IS_AUTH, data} as const)
 
-export default profileReducer
+export default authReducer
