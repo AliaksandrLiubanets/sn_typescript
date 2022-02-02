8d11dc4deeb3a9b9fb11e8fbd52003a1af763e7e
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-02 23:22:21 +0300

    Create api.js with usersAPI and getUsers method in it.

---
+++ src/api/api.js
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+const instance = axios.create({
+    baseURL: 'https://social-network.samuraijs.com/api/1.0/',
+    withCredentials: true,
+    headers: {
+        "API-KEY": "ec259ea8-b888-43af-83e9-f75c638bfe8f"
+    }
+})
+
+
+
+export const usersAPI = {
+    getUsers(currentPage, pageSize) {
+        return instance.get(`users?page=${currentPage}&count=${pageSize}`)
+    }
+}
