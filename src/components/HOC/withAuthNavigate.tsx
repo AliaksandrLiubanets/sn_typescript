89aaff9bd7598ea5e064e825e0368f8a96f7919a
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-08 17:48:36 +0300

    Refactor withAuthNavigate - functional component instead of class withAuthNavigate.tsx

--- src/components/HOC/withAuthNavigate.tsx
+++ src/components/HOC/withAuthNavigate.tsx
@@ -8,17 +8,27 @@ const mapStateToProps = (state: RootStateType): MapStateProps => {
     return {isAuth: state.auth.isAuth}
 }
 
+// export const withAuthNavigate = (Component: any) => {
+//     class NavigateComponent extends React.Component<any> {
+//         render () {
+//             if(!this.props.isAuth) {
+//                 return <Navigate to={'/login'} />
+//             }
+//             return <Component {...this.props}/>
+//         }
+//     }
+//
+//     // let ConnectedComponent = connect(mapStateToProps)(NavigateComponent)
+//
+//     return connect(mapStateToProps)(NavigateComponent)
+// }
+
 export const withAuthNavigate = (Component: any) => {
-    class NavigateComponent extends React.Component<any> {
-        render () {
-            if(!this.props.isAuth) {
-                return <Navigate to={'/login'} />
-            }
-            return <Component {...this.props}/>
+    const NavigateComponent = (props: any) => {
+        if(!props.isAuth) {
+            return <Navigate to={'/login'} />
         }
+        return <Component {...props} />
     }
-
-    // let ConnectedComponent = connect(mapStateToProps)(NavigateComponent)
-
     return connect(mapStateToProps)(NavigateComponent)
 }
