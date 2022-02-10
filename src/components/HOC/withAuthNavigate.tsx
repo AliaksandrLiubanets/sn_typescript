bc8a8fe117c417f517f1215420243b263a34604c
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-10 23:43:29 +0300

    Type withAuthNavigate.tsx

--- src/components/HOC/withAuthNavigate.tsx
+++ src/components/HOC/withAuthNavigate.tsx
@@ -1,34 +1,26 @@
-import React from 'react'
+import React, {ComponentType} from 'react'
 import {Navigate} from 'react-router-dom'
 import {RootStateType} from '../Redux/redux-store'
 import {connect} from 'react-redux'
 
-type MapStateProps = {isAuth: boolean}
-const mapStateToProps = (state: RootStateType): MapStateProps => {
-    return {isAuth: state.auth.isAuth}
+type MapStateProps = {
+    isAuth: boolean
 }
 
-// export const withAuthNavigate = (Component: any) => {
-//     class NavigateComponent extends React.Component<any> {
-//         render () {
-//             if(!this.props.isAuth) {
-//                 return <Navigate to={'/login'} />
-//             }
-//             return <Component {...this.props}/>
-//         }
-//     }
-//
-//     // let ConnectedComponent = connect(mapStateToProps)(NavigateComponent)
-//
-//     return connect(mapStateToProps)(NavigateComponent)
-// }
+const mapStateToProps = (state: RootStateType): MapStateProps => {
+    return {
+        isAuth: state.auth.isAuth,
+    }
+}
 
-export const withAuthNavigate = (Component: any) => {
-    const NavigateComponent = (props: any) => {
-        if(!props.isAuth) {
+export function withAuthNavigate<T>(Component: ComponentType<T>) {
+    debugger
+    const NavigateComponent = (props: MapStateProps) => {
+        const {isAuth, ...rest} = props
+        if(!isAuth) {
             return <Navigate to={'/login'} />
         }
-        return <Component {...props} />
+        return <Component {...rest as T} />
     }
     return connect(mapStateToProps)(NavigateComponent)
 }
