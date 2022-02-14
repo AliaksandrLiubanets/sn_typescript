a8cdb1e7f982a67a4c200b3db787a2db4dcbe975
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-14 23:33:28 +0300

    Fix auth-reducer: add setIsAuthAC and add it in setAuthData and in loginOut with corresponding true and false

--- src/components/Redux/auth-reducer.ts
+++ src/components/Redux/auth-reducer.ts
@@ -2,6 +2,7 @@ import {Dispatch} from 'redux'
 import {authAPI} from '../../api/api'
 
 export const SET_AUTH_DATA = 'sn-typescript/Authorize/SET-AUTH-DATA'
+export const SET_IS_AUTH = 'sn-typescript/Authorize/SET-IS-AUTH'
 
 export type AuthDataType = {
     id: string | null
@@ -33,22 +34,26 @@ const initialState: AuthStateType = {
 const authReducer = (state: AuthStateType = initialState, action: AuthActionsType): AuthStateType => {
     switch (action.type) {
         case SET_AUTH_DATA:
-            return {...state, data: action.data, isAuth: true}
+            return {...state, data: action.data}
+        case SET_IS_AUTH:
+            return {...state, isAuth: action.isAuth}
         default:
             return state
     }
 }
 
 export type AuthType = ReturnType<typeof setAuthDataAC>
+export type SetIsAuthType = ReturnType<typeof setIsAuthAC>
 
 // type AuthType = {
 //     type: typeof SET_AUTH_DATA
 //     data: AuthDataType
 // }
 
-export type AuthActionsType = AuthType
+export type AuthActionsType = AuthType | SetIsAuthType
 
 export const setAuthDataAC = (data: AuthDataType) => ({type: SET_AUTH_DATA, data} as const)
+export const setIsAuthAC = (isAuth: boolean) => ({type: SET_IS_AUTH, isAuth} as const)
 // export const setAuthDataAC = (data: AuthDataType): AuthType => ({type: SET_AUTH_DATA, data})
 
 export const setAuthData = () => (dispatch: Dispatch) => {
@@ -56,6 +61,7 @@ export const setAuthData = () => (dispatch: Dispatch) => {
         .then(response => {
             if(response.data.resultCode === 0) {
                 dispatch(setAuthDataAC(response.data.data))
+                dispatch(setIsAuthAC(true))
             }
         })
 }
@@ -69,11 +75,12 @@ export const login = (payload: LoginPayloadType) => () => {
         })
 }
 
-export const loginOut = () => () => {
+export const loginOut = () => (dispatch: Dispatch) => {
     authAPI.logout()
         .then(response => {
             if(response.data.resultCode === 0) {
                 setAuthData()
+                dispatch(setIsAuthAC(false))
             }
         })
 }
