1faabad4855fc6dfb7b9019ce3407dde6ec13232
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-17 21:34:43 +0300

    Fix validate in validation.ts

--- src/components/FormikFields/SignupForm.jsx
+++ src/components/FormikFields/SignupForm.jsx
@@ -1,41 +1,25 @@
 import React from 'react';
 import { useFormik } from 'formik';
 import {validate} from "./validation";
-
-// export const validate = values => {
-//     const errors = {};
-//
-//     if (!values.firstName) {
-//         errors.firstName = 'Required';
-//     } else if (values.firstName.length > 15) {
-//         errors.firstName = 'Must be 15 characters or less';
-//     }
-//
-//     if (!values.lastName) {
-//         errors.lastName = 'Required';
-//     } else if (values.lastName.length > 20) {
-//         errors.lastName = 'Must be 20 characters or less';
-//     }
-//
-//     if (!values.email) {
-//         errors.email = 'Required';
-//     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
-//         errors.email = 'Invalid email address';
-//     }
-//
-//     return errors;
-// };
+import {login, LoginPayloadType} from "../Redux/auth-reducer";
+import {useDispatch} from "react-redux";
 
 export const SignupForm = () => {
+
+    const dispatch = useDispatch()
+
     const formik = useFormik({
         initialValues: {
-            firstName: '',
-            lastName: '',
             email: '',
+            password: '',
+            rememberMe: true,
+            captcha: true
         },
         validate,
-        onSubmit: values => {
-            alert(JSON.stringify(values, null, 2));
+        onSubmit: (values: LoginPayloadType & ) => {
+            console.log(values)
+            // setSubmitting(false)
+            dispatch(login(values))
         },
     });
     return (
--- src/components/FormikFields/validation.ts
+++ src/components/FormikFields/validation.ts
@@ -1,28 +1,25 @@
-type ValuesType = {
-    firstName?: string
-    lastName?: string
+import {LoginPayloadType} from '../Redux/auth-reducer'
+
+export type ErrorValuesType = {
     email?: string
+    password?: string
+    rememberMe?: string
+    captcha?: string
 }
 
-export const validate = (values: ValuesType) => {
-    const errors: ValuesType = {};
-
-    if (!values.firstName) {
-        errors.firstName = 'Required';
-    } else if (values.firstName.length > 15) {
-        errors.firstName = 'Must be 15 characters or less';
-    }
-
-    if (!values.lastName) {
-        errors.lastName = 'Required';
-    } else if (values.lastName.length > 20) {
-        errors.lastName = 'Must be 20 characters or less';
-    }
+export const validate = (values: LoginPayloadType) => {
+    const errors: ErrorValuesType = {};
 
     if (!values.email) {
         errors.email = 'Required';
     } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
         errors.email = 'Invalid email address';
+    }
+
+    if (!values.password) {
+        errors.password = 'Required';
+    } else if (values.password.length > 15) {
+        errors.password = 'Must be 15 characters or less';
     }
 
     return errors;
