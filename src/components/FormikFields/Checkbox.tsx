4d60b04061f2ce42477ab32b9b176a89ecc92e4b
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-18 19:43:39 +0300

    Refactor Checkbox.tsx in FormikField directory

--- src/components/FormikFields/Checkbox.tsx
+++ src/components/FormikFields/Checkbox.tsx
@@ -1,5 +1,6 @@
-import {FC} from 'react'
+import React, {FC} from 'react'
 import s from './FormikFields.module.css'
+import {useField} from 'formik'
 
 type CheckboxType = {
     labelName: string
@@ -7,20 +8,35 @@ type CheckboxType = {
     type: string
 }
 
-export const Checkbox: FC<CheckboxType> = ({
-                                               // field,
-                                               type,
-                                               name,
-                                               labelName
-                                           }) => {
-    return <div>
-        <label className={s.checkbox_label}>
-            <input
-                //{...field}
-                name={name}
-                className=""
-                type={type}/>
-            <span className="">{labelName}</span>
-        </label>
-    </div>
-}
+// export const Checkbox: FC<CheckboxType> = ({
+//                                                // field,
+//                                                type,
+//                                                name,
+//                                                labelName
+//                                            }) => {
+//     return <div>
+//         <label className={s.checkbox_label}>
+//             <input
+//                 //{...field}
+//                 name={name}
+//                 className=""
+//                 type={type}/>
+//             <span className="">{labelName}</span>
+//         </label>
+//     </div>
+// }
+
+export const Checkbox = ({ labelName, ...props }: CheckboxType) => {
+    const [field, meta] = useField({ ...props, type: "checkbox" });
+    return (
+        <>
+            <label className={s.checkbox_label}>
+                <input {...field} {...props} type="checkbox" />
+                {labelName}
+            </label>
+            {meta.touched && meta.error ? (
+                <div className={s.checkbox_error}>{meta.error}</div>
+            ) : null}
+        </>
+    );
+};
--- src/components/FormikFields/SignupForm.tsx
+++ src/components/FormikFields/SignupForm.tsx
@@ -61,3 +61,140 @@ export const SignupForm = () => {
         </form>
     );
 };
+
+import React, { useEffect } from "react";
+import ReactDOM from "react-dom";
+import { Formik, Form, useField, useFormikContext } from "formik";
+import * as Yup from "yup";
+import styled from "@emotion/styled";
+import "./styles.css";
+import "./styles-custom.css";
+
+const MyTextInput = ({ label, ...props }) => {
+    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+    // which we can spread on <input> and alse replace ErrorMessage entirely.
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <label htmlFor={props.id || props.name}>{label}</label>
+            <input className="text-input" {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <div className="error">{meta.error}</div>
+            ) : null}
+        </>
+    );
+};
+
+
+
+// Styled components ....
+const StyledSelect = styled.select`
+  color: var(--blue);
+`;
+
+const StyledErrorMessage = styled.div`
+  font-size: 12px;
+  color: var(--red-600);
+  width: 400px;
+  margin-top: 0.25rem;
+  &:before {
+    content: "❌ ";
+    font-size: 10px;
+  }
+  @media (prefers-color-scheme: dark) {
+    color: var(--red-300);
+  }
+`;
+
+const StyledLabel = styled.label`
+  margin-top: 1rem;
+`;
+
+const MySelect = ({ label, ...props }) => {
+    // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+    // which we can spread on <input> and alse replace ErrorMessage entirely.
+    const [field, meta] = useField(props);
+    return (
+        <>
+            <StyledLabel htmlFor={props.id || props.name}>{label}</StyledLabel>
+            <StyledSelect {...field} {...props} />
+            {meta.touched && meta.error ? (
+                <StyledErrorMessage>{meta.error}</StyledErrorMessage>
+            ) : null}
+        </>
+    );
+};
+
+// And now we can use these
+const SignupForm = () => {
+
+    const dispatch = useDispatch()
+
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+            rememberMe: false,
+            captcha: true
+        },
+        validate,
+        onSubmit: (values) => {
+            dispatch(login(values))
+        },
+    });
+
+    return (
+        <>
+            <h1>LOGIN!</h1>
+            <Formik
+                initialValues={{
+                    email: '',
+                    password: '',
+                    rememberMe: false, // added for our checkbox
+                    captcha: true, // default value captcha
+                }}
+                validationSchema={validate}
+                onSubmit={ (values) => {
+                    dispatch(login(values))
+                }}
+                // onSubmit={async (values, { setSubmitting }) => {
+                //     await new Promise(r => setTimeout(r, 500));
+                //     setSubmitting(false);
+                // }}
+            >
+                <Form>
+                    <MyTextInput
+                        label="First Name"
+                        name="firstName"
+                        type="text"
+                        placeholder="Jane"
+                    />
+                    <MyTextInput
+                        label="Last Name"
+                        name="lastName"
+                        type="text"
+                        placeholder="Doe"
+                    />
+                    <MyTextInput
+                        label="Email Address"
+                        name="email"
+                        type="email"
+                        placeholder="jane@formik.com"
+                    />
+                    <MySelect label="Job Type" name="jobType">
+                        <option value="">Select a job type</option>
+                        <option value="designer">Designer</option>
+                        <option value="development">Developer</option>
+                        <option value="product">Product Manager</option>
+                        <option value="other">Other</option>
+                    </MySelect>
+                    <MyCheckbox name="acceptedTerms">
+                        I accept the terms and conditions
+                    </MyCheckbox>
+
+                    <button type="submit">Submit</button>
+                </Form>
+            </Formik>
+        </>
+    );
+};
