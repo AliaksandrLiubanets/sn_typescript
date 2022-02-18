ead2f18a58d8e23725ce803f0a1f41a75324f1d3
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-02-18 18:42:08 +0300

    Refactor LoginContainer.tsx - delete dispatch and login callback as prop for SignupForm

--- src/components/Login/LoginContainer.tsx
+++ src/components/Login/LoginContainer.tsx
@@ -1,19 +1,13 @@
-import {useDispatch, useSelector} from 'react-redux'
+import {useSelector} from 'react-redux'
 import {RootStateType} from '../Redux/redux-store'
 import {ProfileWithParam} from '../Profile/ProfileWithParam'
 import {SignupForm} from '../FormikFields/SignupForm'
 import React from 'react'
-import {login, LoginPayloadType} from '../Redux/auth-reducer'
 
 export const LoginContainer = () => {
     const isAuth = useSelector<RootStateType, boolean>(state => state.auth.isAuth)
-    const dispatch = useDispatch()
 
-    const dispatchFormValue = (values: LoginPayloadType) => {
-        dispatch(login(values))
-    }
     if (!isAuth) {
-        // return <SignupForm login={dispatchFormValue}/>
         return <SignupForm/>
     }
     return <ProfileWithParam/>
