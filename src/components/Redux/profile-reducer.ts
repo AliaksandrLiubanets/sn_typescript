aaabad579631bf473f992c97f2942e5fd9d446ae
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-03-31 16:10:38 +0300

    Refactor profile-reducer.ts - replace .then with async await, change code structure

--- src/components/Redux/profile-reducer.ts
+++ src/components/Redux/profile-reducer.ts
@@ -9,41 +9,6 @@ export const ADD_CURRENT_VALUE = 'sn-typescript/ProfilePage/ADD-CURRENT-VALUE'
 export const SET_USER_PROFILE = 'sn-typescript/ProfilePage/SET-USER-PROFILE'
 export const SET_STATUS = 'sn-typescript/ProfilePage/SET-STATUS'
 
-export type PostType = {
-    id: string
-    message: string
-    likes: number
-}
-
-export type ContactsType = {
-    github: string
-    vk: string
-    facebook: string
-    instagram: string
-    twitter: string
-    website: string
-    youtube: string
-    mainLink: string
-}
-
-export type ProfileType = {
-    userId: number
-    lookingForAJob: string
-    lookingForAJobDescription: string
-    fullName: string
-    contacts: ContactsType
-    photos: {
-        small: string
-        large: string
-    }
-}
-
-export type ProfilePageType = {
-    textareaCurrentValue: string
-    messagesData: Array<PostType>
-    profile: ProfileType | null
-    status: string | null
-}
 
 export const initialState: ProfilePageType = {
     textareaCurrentValue: '',
@@ -75,7 +40,8 @@ export const profileReducer = (state = initialState, action: ProfilePageActionsT
             }
             return state
         case DELETE_POST:
-            return {...state,
+            return {
+                ...state,
                 messagesData: state.messagesData.filter(post => post.id !== action.postId)
             }
 
@@ -90,6 +56,71 @@ export const profileReducer = (state = initialState, action: ProfilePageActionsT
     }
 }
 
+export default profileReducer
+
+// actions:
+export const addPostAC = () => ({type: ADD_POST} as const)
+export const deletePostAC = (postId: string) => ({type: DELETE_POST, postId} as const)
+export const addCurrentValueAC = (text: string) => ({type: ADD_CURRENT_VALUE, newText: text} as const)
+export const setUserProfileAC = (profile: ProfileType) => ({type: SET_USER_PROFILE, profile} as const)
+export const setStatusProfileAC = (status: string) => ({type: SET_STATUS, status} as const)
+
+
+// thunks:
+export const setUserProfile = (userId: number): ThunkType => async (dispatch) => {
+    const response = await profileAPI.getUserProfile(userId)
+    dispatch(setUserProfileAC(response.data))
+
+}
+
+export const getStatus = (userId: number): ThunkType => async (dispatch) => {
+    const response = await profileAPI.getStatus(userId)
+    dispatch(setStatusProfileAC(response.data))
+}
+
+export const setStatus = (status: string): ThunkType => async (dispatch) => {
+    const response = await profileAPI.setStatus(status)
+    if (response.data.resultCode === 0) {
+        dispatch(setStatusProfileAC(status))
+    }
+}
+
+// actions:
+export type PostType = {
+    id: string
+    message: string
+    likes: number
+}
+export type ContactsType = {
+    github: string
+    vk: string
+    facebook: string
+    instagram: string
+    twitter: string
+    website: string
+    youtube: string
+    mainLink: string
+}
+export type ProfileType = {
+    userId: number
+    lookingForAJob: string
+    lookingForAJobDescription: string
+    fullName: string
+    contacts: ContactsType
+    photos: {
+        small: string
+        large: string
+    }
+}
+export type ProfilePageType = {
+    textareaCurrentValue: string
+    messagesData: Array<PostType>
+    profile: ProfileType | null
+    status: string | null
+}
+
+type ThunkType = ThunkAction<void, RootStateType, unknown, ProfilePageActionsType>
+
 export type AddPostActionType = ReturnType<typeof addPostAC>
 export type DeletePostActionType = ReturnType<typeof deletePostAC>
 export type AddCurrentValueActionType = ReturnType<typeof addCurrentValueAC>
@@ -102,37 +133,3 @@ export type ProfilePageActionsType =
     | SetUserProfileActionType
     | SetStatusActionType
     | DeletePostActionType
-
-export const addPostAC = () => ({type: ADD_POST} as const)
-export const deletePostAC = (postId: string) => ({type: DELETE_POST, postId} as const)
-export const addCurrentValueAC = (text: string) => ({type: ADD_CURRENT_VALUE, newText: text} as const)
-export const setUserProfileAC = (profile: ProfileType) => ({type: SET_USER_PROFILE, profile} as const)
-export const setStatusProfileAC = (status: string) => ({type: SET_STATUS, status} as const)
-
-
-type ThunkType = ThunkAction<void, RootStateType, unknown, ProfilePageActionsType>
-
-export const setUserProfile = (userId: number): ThunkType => (dispatch) => {
-    return profileAPI.getUserProfile(userId)
-        .then(response => {
-            dispatch(setUserProfileAC(response.data))
-        })
-}
-
-export const getStatus = (userId: number): ThunkType => (dispatch) => {
-    return profileAPI.getStatus(userId)
-        .then(response => {
-            dispatch(setStatusProfileAC(response.data))
-        })
-}
-
-export const setStatus = (status: string): ThunkType => (dispatch) => {
-    return profileAPI.setStatus(status)
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(setStatusProfileAC(status))
-            }
-        })
-}
-
-export default profileReducer
