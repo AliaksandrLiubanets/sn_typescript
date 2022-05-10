3d42b61f3b615bc31cacdb04181a52278b838d62
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-05-10 08:59:12 +0300

    Refactor auth-reducer.ts - try/catch/finally instead of .then

--- src/components/Redux/auth-reducer.ts
+++ src/components/Redux/auth-reducer.ts
@@ -1,7 +1,6 @@
 import {authAPI} from '../../api/api'
 import {AppThunk, InferActionTypes} from './redux-store'
 import {appActions} from './app-reducer'
-import {Dispatch} from 'redux'
 import {profileActions} from './profile-reducer'
 import axios from 'axios'
 import {handleServerNetworkError} from '../../utils/handleError'
@@ -17,7 +16,7 @@ const initialState: AuthStateType = {
         login: null
     },
     isAuth: false,
-    avatar: null,
+    avatar: null
 }
 
 const authReducer = (state: StateType = initialState, action: AuthActionsType): StateType => {
@@ -26,7 +25,7 @@ const authReducer = (state: StateType = initialState, action: AuthActionsType): 
         case SET_IS_AUTH:
             return {...state, ...action.payload}
         case SET_AVATAR:
-            if(state.data.id === action.payload.userId) {
+            if (state.data.id === action.payload.userId) {
                 return {...state, avatar: action.payload.avatar}
             } else {
                 return state
@@ -43,52 +42,52 @@ export default authReducer
 export const authActions = {
     setAuthData: (data: AuthDataType) => ({type: SET_AUTH_DATA, payload: {data}} as const),
     setIsAuth: (isAuth: boolean) => ({type: SET_IS_AUTH, payload: {isAuth}} as const),
-    setIAvatar: (avatar: string | null, userId: number | null) => ({type: SET_AVATAR, payload: {avatar, userId}} as const),
+    setIAvatar: (avatar: string | null, userId: number | null) => ({
+        type: SET_AVATAR,
+        payload: {avatar, userId}
+    } as const)
 }
 
 // thunks:
-export const getAuthData = () => (dispatch: Dispatch) => {
+export const getAuthData = (): AppThunk => async (dispatch) => {
     dispatch(appActions.setInitialize(true))
-        authAPI.me()
-            .then((response) => {
-            if (response.data.resultCode === 0) {
-                dispatch(authActions.setAuthData(response.data.data))
-                dispatch(authActions.setIsAuth(true))
-            } else {
-                dispatch(appActions.setAppError(response.data.messages[0]))
-            }
-        })
-            .catch(err => {
-                if (axios.isAxiosError(err) && err.response) {
-                    dispatch(appActions.setAppError(err.response.data.message))
-                }
-            })
-            .finally(() => {
-                dispatch(appActions.setInitialize(false))
-            })
+    try {
+        const response = await authAPI.me()
+        if (response.data.resultCode === 0) {
+            dispatch(authActions.setAuthData(response.data.data))
+            dispatch(authActions.setIsAuth(true))
+        } else {
+            dispatch(appActions.setAppError(response.data.messages[0]))
+        }
+    }
+    catch (e) {
+        if (axios.isAxiosError(e) && e.response) {
+            dispatch(appActions.setAppError(e.response.data.message))
+        }
+    }
+    finally {
+        dispatch(appActions.setInitialize(false))
+    }
 }
 
-export const login = (payload: LoginPayloadType): AppThunk => (dispatch) => {
+export const login = (payload: LoginPayloadType): AppThunk => async (dispatch) => {
     dispatch(appActions.setIsLoading(true))
-    return authAPI.login(payload)
-        .then(response => {
-            if (response.data.resultCode === 0) {
-                dispatch(getAuthData())
-            } else {
-                if (response.data.messages.length) {
-                    const error: string = response.data.messages[0]
-                    dispatch(appActions.setAppError(error))
-                    dispatch(appActions.setIsLoading(false))
-                }
-            }
-        })
-        .catch(e => {
-            handleServerNetworkError(dispatch, e as Error)
-        })
-        .finally(() => {
+    try {
+        const response = await authAPI.login(payload)
+        if (response.data.resultCode === 0) {
+            dispatch(getAuthData())
+        } else if (response.data.messages.length) {
+            const error: string = response.data.messages[0]
+            dispatch(appActions.setAppError(error))
             dispatch(appActions.setIsLoading(false))
-        })
+        }
+    } catch (e) {
+        handleServerNetworkError(dispatch, e as Error)
+    } finally {
+        dispatch(appActions.setIsLoading(false))
+    }
 }
+
 
 export const loginOut = (): AppThunk => async (dispatch) => {
     dispatch(appActions.setIsLoading(true))
@@ -99,11 +98,9 @@ export const loginOut = (): AppThunk => async (dispatch) => {
             dispatch(profileActions.setUserProfile(null))
             dispatch(authActions.setIAvatar(null, null))
         }
-    }
-    catch (e) {
+    } catch (e) {
         handleServerNetworkError(dispatch, e as Error)
-    }
-    finally {
+    } finally {
         dispatch(appActions.setIsLoading(false))
     }
 }
