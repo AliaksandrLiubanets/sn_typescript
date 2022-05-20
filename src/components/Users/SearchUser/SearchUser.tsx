31c105a6bfa0a00619225adc9bb356d690e2f6bb
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-05-20 21:43:37 +0300

    Use Select in SearchUser instead of Checkbox

--- src/components/Users/SearchUser/SearchUser.tsx
+++ src/components/Users/SearchUser/SearchUser.tsx
@@ -5,33 +5,54 @@ import {FC, useEffect, useState} from 'react'
 import {useDispatch, useSelector} from 'react-redux'
 import {getUsers, SearchType} from '../../Redux/users-reducer'
 import {RootStateType} from '../../Redux/redux-store'
-import {Checkbox} from '../../common/Checkbox/Checkbox'
+import {Select} from '../../common/Select/Select'
 
 type PropsType = {
     searchParams: SearchType
 }
 
+const friendArray = ['all', 'subscribed', 'unsubscribed']
+
 export const SearchUser: FC<PropsType> = ({searchParams}) => {
-    const [checked, setChecked] = useState<boolean | null>(searchParams.friend)
+
+    const reduxFriend = useSelector<RootStateType, boolean | null>(state => state.usersPage.searchParams.friend)
+
+    let initialfriend: 'all' | 'subscribed' |'unsubscribed'
+    if(reduxFriend === true) {
+        initialfriend = 'subscribed'
+    } else if (reduxFriend === false) {
+        initialfriend = 'unsubscribed'
+    } else {
+        initialfriend = 'all'
+    }
+
     const [name, setName] = useState<string>(searchParams.term)
+    const [friend, setFriend] = useState<string>(initialfriend)
 
     useEffect(() => {
-        setChecked(searchParams.friend)
         setName(searchParams.term)
-    }, [searchParams.term, searchParams.friend])
+    }, [searchParams.term])
 
     const pageSize = useSelector<RootStateType, number>(state => state.usersPage.pageSize)
 
     const dispatch = useDispatch()
 
+    let isFriend: boolean | null
+    if(friend === 'subscribed') {
+        isFriend = true
+    } else if (friend === 'unsubscribed') {
+        isFriend = false
+    } else {
+        isFriend = null
+    }
+
     const search = () => {
-        dispatch(getUsers(1, pageSize, name, checked))
+        dispatch(getUsers(1, pageSize, name, isFriend))
     }
 
     return <div className={s.search_block}>
         <InputField placeholder={'search user by name'} onChangeText={setName} value={name} onEnter={search}/>
-        <Checkbox children={'only subscribed'} onChangeChecked={setChecked}
-                  checked={checked ? checked : undefined}/>
+        <Select options={friendArray} onChangeOption={setFriend} value={friend}/>
         <Button label={'Search'} onClickHandler={search}/>
     </div>
 }
