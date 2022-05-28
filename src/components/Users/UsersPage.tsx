eec1a2f56513fdab245cc2dafad7720b92af3b06
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-05-28 19:23:48 +0300

    Fix condition for users render in UsersPage or Spinner - add isLoading

--- src/components/Users/UsersPage.tsx
+++ src/components/Users/UsersPage.tsx
@@ -1,7 +1,12 @@
 import {useDispatch, useSelector} from 'react-redux'
 import {getUsers} from '../Redux/users-reducer'
 import React, {useEffect, useState} from 'react'
-import {getCurrentPageSelector, getPageSizeSelector, getUsersSelector} from '../../selectors/users-selectors'
+import {
+    getCurrentPageSelector,
+    getPageSizeSelector,
+    getUsersSelector,
+    isLoadingSelector
+} from '../../selectors/users-selectors'
 import {Spinner} from '../common/Spinner/Spinner'
 import {Users} from './Users'
 
@@ -10,15 +15,16 @@ export const UsersPage = () => {
 
     const currentPage = useSelector(getCurrentPageSelector)
     const pageSize = useSelector(getPageSizeSelector)
+    const isLoading = useSelector(isLoadingSelector)
     const users = useSelector(getUsersSelector)
 
     const dispatch = useDispatch()
 
     useEffect(() => {
         dispatch(getUsers(currentPage, pageSize))
-    }, [])
+    }, [dispatch])
 
-    if(!isOnSearchClick && users.length === 0) {
+    if((!isOnSearchClick && users.length === 0) || isLoading) {
         return <Spinner />
     }
 
