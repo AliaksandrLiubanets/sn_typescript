aa5ed6f998d9684fe350af8a4cfcd61f3370b5f8
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-05-31 11:21:59 +0300

    Using useLocation Add parsed data to searchUsers in useEffect in UsersPage.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -57,9 +57,10 @@ class App extends React.Component<AppPropsType> {
                                 <Route path="/news" element={<News/>}/>
                                 <Route path="/music" element={<Music/>}/>
                                 <Route path="/users" element={
-                                    <NavigateToLogin>
+                                    // <NavigateToLogin>
                                         <UsersPage/>
-                                    </NavigateToLogin>}/>
+                                    // </NavigateToLogin>
+                                }/>
                                 <Route path="/settings" element={<Settings/>}/>
                                 <Route path="/login" element={<Login/>}/>
                                 <Route path="*" element={<Page404/>}/>
--- src/components/Users/UsersPage.tsx
+++ src/components/Users/UsersPage.tsx
@@ -11,6 +11,7 @@ import {Spinner} from '../common/Spinner/Spinner'
 import {Users} from './Users'
 import {useNavigate, createSearchParams, useLocation} from 'react-router-dom'
 
+
 export const UsersPage = React.memo(() => {
 
     const currentPage = useSelector(getCurrentPageSelector)
@@ -23,20 +24,48 @@ export const UsersPage = React.memo(() => {
     const setSearchToggle = useCallback((toggle: boolean) => dispatch(usersActions.setSearchToggle(toggle)), [dispatch])
 
     const navigate = useNavigate()
+    const location = useLocation()
 
     useEffect(() => {
-        dispatch(getUsers(currentPage, pageSize))
+        debugger
+        const queryString = require('query-string');
+        const parsed: parsedUsersUrlType = queryString.parse(location.search)
+
+        let actualPage = currentPage
+        let actualFilter = filter
+
+        if(parsed.page) actualPage = Number(parsed.page)
+        if(!!parsed.term) actualFilter = {...actualFilter, term: parsed.term}
+
+        let friend: boolean | null
+        switch (parsed.friend) {
+            case 'true':
+                friend = true
+                break
+            case 'false':
+                friend = false
+                break
+            default: friend = null
+        }
+        if(!!parsed.friend) actualFilter = {...actualFilter,
+            // friend: parsed.friend === 'true' ? true : (parsed.friend === 'false' ? false : null)
+            friend: friend
+        }
+
+        dispatch(searchUsers(actualPage, pageSize, actualFilter))
         return () => {
             setSearchToggle(false)
         }
     }, [])
 
-    useEffect(() => {
-        navigate({
-            pathname: '/users',
-            search: `?term=${filter.term}&friend=${filter.friend}&page=${currentPage}`
-        })
-    }, [filter, currentPage])
+    const url = 'http://localhost:3000/sn_typescript#/users?term=den&friend=true&page=1'
+
+    // useEffect(() => {
+    //     navigate({
+    //         pathname: '/users',
+    //         search: `?term=${filter.term}&friend=${filter.friend}&page=${currentPage}`
+    //     })
+    // }, [filter, currentPage])
 
     if (!isSearchToggle || isLoading) {
         return <Spinner/>
@@ -44,3 +73,9 @@ export const UsersPage = React.memo(() => {
 
     return <Users isSearchToggle={isSearchToggle} setSearchToggle={setSearchToggle}/>
 })
+
+type parsedUsersUrlType = {
+    term?: string
+    friend?: string
+    page?: string
+}
