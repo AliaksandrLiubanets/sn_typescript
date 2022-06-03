767339da1eabb3c224b4d2529e6257bac25f8a77
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-06-03 21:05:32 +0300

    Add Spinner to Dialogs.tsx while loading process is going

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -4,26 +4,36 @@ import {Outlet} from 'react-router-dom'
 import {useSelector} from 'react-redux'
 import {DialogType} from '../Redux/dialogs-reducer'
 import p from '../Profile/Profile.module.css'
-import {dialogsSelector} from '../../selectors/users-selectors'
+import {appSelector, dialogsSelector} from '../../selectors/users-selectors'
+import {Spinner} from '../common/Spinner/Spinner'
 
 export const Dialogs = () => {
 
     const {dialogs} = useSelector(dialogsSelector)
+    const {isLoading} = useSelector(appSelector)
+
 
     const dialogsItems = dialogs.map((d: DialogType) => <DialogsItem key={d.id}
-                                                                           name={d.name}
-                                                                           id={d.id}
-                                                                           ava={d.ava}/>)
+                                                                     name={d.name}
+                                                                     id={d.id}
+                                                                     ava={d.ava}/>)
 
-    return <div className={`${p.page_block} ${s.dialogs__block} `}>
-        <div className={s.dialogs}>
-            <h4>DIALOGS</h4>
-            {dialogsItems}
-        </div>
-        <div>
-            <Outlet/>
-        </div>
-    </div>
+    return <>
+        {
+            isLoading
+                ? <Spinner/>
+                : <div className={`${p.page_block} ${s.dialogs__block} `}>
+                    <div className={s.dialogs}>
+                        <h4>DIALOGS</h4>
+                        {dialogsItems}
+                    </div>
+                    <div>
+                        <Outlet/>
+                    </div>
+                </div>
+        }
+
+    </>
 }
 
 
