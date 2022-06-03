49786acb61590285ebeb855811b39c14a1df8761
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-06-03 18:24:13 +0300

    Fix NavigateToLogin.tsx - add spinner while app is initializing

--- src/components/common/HOC/NavigateToLogin.tsx
+++ src/components/common/HOC/NavigateToLogin.tsx
@@ -1,16 +1,22 @@
 import {Navigate, useLocation} from 'react-router-dom'
 import {FC} from 'react'
 import {useSelector} from 'react-redux'
-import {isAuthSelector} from '../../../selectors/users-selectors'
+import {appSelector, authSelector} from '../../../selectors/users-selectors'
+import {Spinner} from '../Spinner/Spinner'
 
 export const NavigateToLogin: FC = ({children}) => {
     const location = useLocation()
-    console.log('NavigateToLogin: ', location)
-    const isAuth = useSelector(isAuthSelector)
+    const {isAuth} = useSelector(authSelector)
+    const {isInitializing} = useSelector(appSelector)
 
-    if (!isAuth) {
-        return <Navigate to={'/login'} state={{from: location}}/>
+    if (isInitializing) {
+        return <Spinner />
     }
 
+    if (!isAuth ) {
+        return <Navigate to={'/login'}
+            state={{from: location}}
+        />
+    }
     return <>{children}</>
 }
