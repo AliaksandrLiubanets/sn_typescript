ba48802f03785cb828e66924d58ed69d1c6c5da8
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-06-22 17:28:27 +0300

    Create chatReducer to redux-store.ts

--- src/components/Redux/redux-store.ts
+++ src/components/Redux/redux-store.ts
@@ -6,6 +6,7 @@ import usersReducer, {UsersAT} from './users-reducer'
 import authReducer, {AuthActionsType} from './auth-reducer'
 import thunk, {ThunkAction} from 'redux-thunk'
 import appReducer, {AppActionsType} from './app-reducer'
+import chatReducer, {ChatActionsType} from './chat-reducer'
 
 const rootReducer = combineReducers({
     profilePage: profileReducer,
@@ -14,13 +15,14 @@ const rootReducer = combineReducers({
     sidebar: sidebarReducer,
     auth: authReducer,
     app: appReducer,
+    chat: chatReducer,
 })
 
 const store = createStore(rootReducer, applyMiddleware(thunk))
 
 export type RootStateType = ReturnType<typeof rootReducer>
 export type InferActionTypes<T> = T extends { [keys: string]: (...args: any[]) => infer U } ? U : never
-export type ActionsType = AppActionsType | AuthActionsType | DialogsActionsType | ProfileActionsType | UsersAT
+export type ActionsType = AppActionsType | AuthActionsType | DialogsActionsType | ProfileActionsType | UsersAT | ChatActionsType
 export type AppThunk<ReturnType = void> = ThunkAction<ReturnType, RootStateType, unknown, ActionsType>
 export type AppStoreType = typeof store
 
