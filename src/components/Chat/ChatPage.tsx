dc0d733ac22f21bf17927b5a902f680d428912e1
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-06-24 12:06:40 +0300

    Add style page_block for AddMessage

--- src/components/Chat/ChatPage.tsx
+++ src/components/Chat/ChatPage.tsx
@@ -4,6 +4,10 @@ import {useDispatch, useSelector} from 'react-redux'
 import {sendMessage, startMessagesListening, stopMessagesListening} from '../Redux/chat-reducer'
 import {chatSelector} from '../../selectors/selectors'
 import p from '../Profile/Profile.module.css'
+import s from '../common/Message/Message.module.css'
+import {MessageAuthor} from '../common/Message/MessageAuthor'
+import {MessageText} from '../common/Message/MessageText'
+import {MessageAvatar} from '../common/Message/MessageAvatar'
 
 const ChatPage: FC = () => {
     return <div>
@@ -65,21 +69,21 @@ export const Messages: FC = () => {
 type MessageType = { message: ChatMessageAPIType }
 
 export const Message: FC<MessageType> = React.memo(({message}) => {
-    return <div>
-        <img src={message.photo} style={{width: '30px'}} alt={'avatar'}/><b>{message.userName}</b>
-        <br/>
-        {message.message}
-        <hr/>
-    </div>
-
-    // return <div className={s.message_block}>
-    //     <div className={s.ava_name_message}>
-    //         <MessageAvatar url={message.photo}/>
-    //         <MessageAuthor author={message.userName}/>
-    //     </div>
-    //     <div className={s.angle}></div>
-    //     <MessageText text={message.message}/>
+    // return <div>
+    //     <img src={message.photo} style={{width: '30px'}} alt={'avatar'}/><b>{message.userName}</b>
+    //     <br/>
+    //     {message.message}
+    //     <hr/>
     // </div>
+
+    return <div className={s.message_block}>
+        <div className={s.ava_name_message}>
+            <MessageAvatar url={message.photo}/>
+            <MessageAuthor author={message.userName}/>
+        </div>
+        <div className={s.angle}></div>
+        <MessageText text={message.message}/>
+    </div>
 })
 
 
