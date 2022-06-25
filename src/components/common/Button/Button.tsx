b0381684265f1bdf8ffad8334b207a7909909ff3
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-06-25 19:57:39 +0300

    Fix props properties in Button.tsx

--- src/components/common/Button/Button.tsx
+++ src/components/common/Button/Button.tsx
@@ -2,12 +2,27 @@ import React, {FC} from 'react'
 
 type TextareaProps = {
     label: string
-    onClickHandler: () => void
+    onClickSubmit?: (message: string) => void
+    onClickHandler?: () => void
+    setMessage?: (message: string) => void
+    disabled?: boolean
+    message?: string
 }
 
-export const Button: FC<TextareaProps> = ({onClickHandler, label, ...props}) => {
+export const Button: FC<TextareaProps> = ({onClickSubmit,
+                                              label,
+                                              disabled,
+                                              message,
+                                              setMessage,
+                                              onClickHandler}) => {
+
+    const onSubmit = () => {
+        onClickHandler && onClickHandler()
+        onClickSubmit && onClickSubmit(message ? message : '')
+        setMessage && setMessage('')
+    }
 
     return  <div>
-        <button onClick={onClickHandler}>{label}</button>
+        <button disabled={disabled} onClick={onSubmit}>{label}</button>
     </div>
 }
