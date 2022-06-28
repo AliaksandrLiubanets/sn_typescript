c3fd47d7c667656977a407e52b21aafd3c21b77d
AliaksandrLiubanets <aliaksandr.liubanets@gmail.com>
2022-06-28 20:17:26 +0300

    Refactor Message.tsx

--- src/components/Dialogs/MessageField/Message.tsx
+++ src/components/Dialogs/MessageField/Message.tsx
@@ -1,21 +1,22 @@
 import s from '../../common/Message/Message.module.css'
-import {FC} from 'react'
+import React, {FC} from 'react'
 import {MessageAvatar} from '../../common/Message/MessageAvatar'
 import {MessageAuthor} from '../../common/Message/MessageAuthor'
 import {MessageText} from '../../common/Message/MessageText'
-import {MessageType} from '../../Redux/dialogs-reducer'
+import {ChatMessageAPIType} from '../../../api/chat-api'
 
-type MessagePropsType = {
-    message: MessageType
+type MessageType = {
+    message: ChatMessageAPIType
 }
 
-export const Message: FC<MessagePropsType> = ({message}) => {
+export const Message: FC<MessageType> = React.memo(({message}) => {
+
     return <div className={s.message_block}>
-        <div className={s.ava_name}>
-            <MessageAvatar url={message.url}/>
-            <MessageAuthor author={message.author}/>
+        <div className={s.ava_name_message}>
+            <MessageAvatar url={message.photo}/>
+            <MessageAuthor author={message.userName}/>
         </div>
-        <div className={s.angle}></div>
-        <MessageText text={message.text}/>
+
+        <MessageText text={message.message}/>
     </div>
-}
+})
